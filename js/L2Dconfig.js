6aea4ef9c76cfcced575a3540dd0b2a401b047ad
Steven Yan <steven_nagisa@qq.com>
2021-12-03 09:41:41 +0800

    Site updated: 2021-12-03 09:41:40

---
+++ js/L2Dconfig.js
@@ -0,0 +1,10 @@
+L2Dwidget.init({
+    "display": {
+        "superSample": 2,
+        "width": 150,
+        "height": 300,
+        "position": "right",
+        "hOffset": 50,
+        "vOffset": 0
+    }
+});
